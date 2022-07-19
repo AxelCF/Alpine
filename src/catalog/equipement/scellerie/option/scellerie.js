2d59cb9219bd5dc2f4ea3941054d7af9c4878b81
hugo <hugo.cifor@gmail.com>
2022-07-19 14:39:18 +0200

    recreation des dossiers

---
+++ src/catalog/equipement/scellerie/option/scellerie.js
@@ -0,0 +1,43 @@
+import { LEGENDE, SINGLE_OPTION } from "../../../../utils/constants";
+
+export const SCELLERIE_SIEGES_BAQUETS_CUIR_NOIR_DINAMICA  =  {
+    type: SINGLE_OPTION,
+    label: 'Sièges baquets en cuir noir et Dinamica',
+    slug: 'Sièges-baquets-en-cuir-noir-et-Dinamica',
+    price: 0,
+    default: true,
+    selected: true,
+    model:PURE
+    // Si une option n'est disponible que pour un seul des deux modèles, le spécifier au format 'model: PURE' par exemple
+}
+
+export const SCELLERIE_SIEGES_CUIR_NOIR_PERFORE = {
+    type: SINGLE_OPTION,
+    label: 'Sièges Confort en cuir noir perforé',
+    slug: 'Sièges-Confort-en-cuir-noir-perforé',
+    price: 800,
+    default: false,
+    selected: false,
+    model:PURE
+
+}
+
+export const SCELLERIE_SIEGES_CONFORT_EN_CUIR_NOIR = {
+    type: SINGLE_OPTION,
+    label: 'Sièges Confort en cuir noir',
+    slug: 'Sièges-Confort-en-cuir-noir',
+    price: 0,
+    default: true,
+    selected: true,
+    model:LEGENDE    
+}
+
+export const SCELLERIE_SIEGES_CONFORT_EN_CUIR_BRUN = {
+    type: SINGLE_OPTION,
+    label: 'Sièges Confort en cuir brun',
+    slug: 'Sièges-Confort-en-cuir-brun',
+    price: 800,
+    default: true,
+    selected: true,
+    model:LEGENDE
+}
