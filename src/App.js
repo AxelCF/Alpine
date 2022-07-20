d6291e69cc2aea69f6ea71aa03b0f60305560412
AxelCF <95211943+AxelCF@users.noreply.github.com>
2022-07-20 09:20:21 +0200

    poge index finie

--- src/App.js
+++ src/App.js
@@ -3,12 +3,11 @@ import logo from './image alpine/sources-homepage/logo/logo-white.png';
 import demo from './image alpine/sources-homepage/A110/Presentation_desktop-1.png'
 import legende from './image alpine/sources-homepage/versions/ALPINE-LEGENDE-1.png'
 import pure from './image alpine/sources-homepage/versions/ALPINE-PURE-1.png'
-import { Card, Button, Stack } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Card } from 'react-bootstrap';
 import ReadMoreReact from 'read-more-react';
 import React from 'react';
 import './App.css';
-import CardHeader from 'react-bootstrap/esm/CardHeader';
 
 function App() {
   return (
@@ -20,12 +19,22 @@ function App() {
         <p><DemoClass/></p>
         <h1>Configurez votre ALPINE</h1>
         <img src={demo} className="alpine" alt="car" />
-      <Card className='card' style={{ background: 'none' }}>
-        <Card.Img variant="top" src={legende} />
-        <Card.Body>
-          <Card.Title>LEGENDE</Card.Title>
-        </Card.Body>
-      </Card>
+
+      <div className='container'>
+        <Card className='card' style={{ background: 'none' }}>
+          <Card.Img variant="top" src={legende} />
+          <Card.Body>
+            <Card.Title>LEGENDE</Card.Title>
+          </Card.Body>
+        </Card>
+
+        <Card className='card' style={{ background: 'none' }}>
+          <Card.Img variant="top" src={pure} />
+          <Card.Body>
+            <Card.Title>PURE</Card.Title>
+          </Card.Body>
+        </Card>
+      </div>
     </div>
 
 
@@ -36,7 +45,7 @@ class DemoClass extends React.Component {
  
   render() { 
       return (
-          <ReadMoreReact text={"Texte présentation de la marque : Fondée en 1955, Alpine s’est affirmée au fil des ans avec ses voitures de sport à la française. En 2018, la marque présente la nouvelle A110, une voiture de sport fidèle aux principes intemporels d’Alpine en matière de compacité, de légèreté, d'agilité et de plaisir de conduite. En 2021, Alpine Cars, Renault Sport Cars et Renault Sport Racing se réunissent sous la bannière Alpine. Elle devient ainsi la marque à la pointe de l’innovation sportive du Groupe Renault, bénéficiant de l’héritage et du savoir-faire de son usine historique de Dieppe ainsi que de la maîtrise de l’ingénierie des équipes de Formule 1 et de Renault Sport Cars. Un « garage de rêve » composé de voitures de sport électriques, exclusives et futuristes, génératrices de rêve mais destinées à la vraie vie, constituera bientôt la gamme de la marque."}
+          <ReadMoreReact text={"Fondée en 1955, Alpine s’est affirmée au fil des ans avec ses voitures de sport à la française. En 2018, la marque présente la nouvelle A110, une voiture de sport fidèle aux principes intemporels d’Alpine en matière de compacité, de légèreté, d'agilité et de plaisir de conduite. En 2021, Alpine Cars, Renault Sport Cars et Renault Sport Racing se réunissent sous la bannière Alpine. Elle devient ainsi la marque à la pointe de l’innovation sportive du Groupe Renault, bénéficiant de l’héritage et du savoir-faire de son usine historique de Dieppe ainsi que de la maîtrise de l’ingénierie des équipes de Formule 1 et de Renault Sport Cars. Un « garage de rêve » composé de voitures de sport électriques, exclusives et futuristes, génératrices de rêve mais destinées à la vraie vie, constituera bientôt la gamme de la marque."}
               min={308}
               ideal={308}
               max={600}
