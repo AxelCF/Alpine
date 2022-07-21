696ad65588074f9dd4ec53bae3e17674902a0f76
sarah <95212766+deveauxsarah@users.noreply.github.com>
2022-07-21 12:31:27 +0200

    maj image

--- src/stepPage.js
+++ src/stepPage.js
@@ -1,22 +1,22 @@
-import logo from './image alpine/sources-homepage/logo/logo-white.png';
+import logo from './image alpine/sources-homepage/logo/logo.png';
 import flecheGreen from './image alpine/sources-homepage/logo/flecheGreen.png';
 import flecheYellow from './image alpine/sources-homepage/logo/flecheYellow.png';
+import ShowCar2 from './image alpine/configurateur/modele/legende/ShowCar2.jpg';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import React from 'react';
 import './StepPage.css';
 
 function StepPage() {
   return (
-    <div>
+    <div className='StepPage'>
+      <div className='header'>
       <header className="App-header">
         <img className="logoAlpine" src={logo} alt="logo" />
       </header>
       <h1>LEGENDE</h1>
-
       <div className='progressStep'>
         <div className='State'>
-        <div className='NumberStep'>1</div>
-        <img className='SingleProgressStep' src={flecheGreen} alt="flecheGreen"/>
+          <div className='NumberStep'>1</div>
+          <img className='SingleProgressStep' src={flecheGreen} alt="flecheGreen"/>
         </div>
         <div className='State'> 
           <div className='NumberStep'>2</div>
@@ -38,10 +38,15 @@ function StepPage() {
           <div className='NumberStep'>6</div>
           <img className='SingleProgressStep' src={flecheYellow} alt="flecheYellow"/>
         </div>
-        
       </div>
-  </div>
-  
+      </div>
+      <div className='ShowCar'>
+        <img className="ShowCar2" src={ShowCar2} alt="modele" />
+      </div>
+
+    </div>
+     
+    
 );
 }
 
