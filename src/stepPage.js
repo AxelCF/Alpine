fe241314fcb59687bb9b682f828096c677184b85
sarah <95212766+deveauxsarah@users.noreply.github.com>
2022-07-22 09:38:03 +0200

    maj

--- src/stepPage.js
+++ src/stepPage.js
@@ -1,7 +1,10 @@
 import logo from './image alpine/sources-homepage/logo/logo.png';
 import flecheGreen from './image alpine/sources-homepage/logo/flecheGreen.png';
-import flecheYellow from './image alpine/sources-homepage/logo/flecheYellow.png';
+import flecheBlue from './image alpine/sources-homepage/logo/flecheBlue.png';
 import ShowCar2 from './image alpine/configurateur/modele/legende/ShowCar2.jpg';
+import blanc from './image alpine/configurateur/couleurs/selection/blanc.jpg';
+import bleu from './image alpine/configurateur/couleurs/selection/bleu.jpg';
+import noir from './image alpine/configurateur/couleurs/selection/noir.jpg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './StepPage.css';
 
@@ -20,28 +23,33 @@ function StepPage() {
         </div>
         <div className='State'> 
           <div className='NumberStep'>2</div>
-          <img className='SingleProgressStep' src={flecheYellow} alt="flecheYellow"/>
+          <img className='SingleProgressStep' src={flecheBlue} alt="flecheBlue"/>
         </div>
         <div className='State'> 
           <div className='NumberStep'>3</div>
-          <img className='SingleProgressStep' src={flecheYellow} alt="flecheYellow"/>
+          <img className='SingleProgressStep' src={flecheBlue} alt="flecheBlue"/>
         </div>
         <div className='State'> 
           <div className='NumberStep'>4</div>
-          <img className='SingleProgressStep' src={flecheYellow} alt="flecheYellow"/>
+          <img className='SingleProgressStep' src={flecheBlue} alt="flecheBlue"/>
         </div>
         <div className='State'> 
           <div className='NumberStep'>5</div>
-          <img className='SingleProgressStep' src={flecheYellow} alt="flecheYellow"/>
+          <img className='SingleProgressStep' src={flecheBlue} alt="flecheBlue"/>
         </div>
         <div className='State'> 
           <div className='NumberStep'>6</div>
-          <img className='SingleProgressStep' src={flecheYellow} alt="flecheYellow"/>
+          <img className='SingleProgressStep' src={flecheBlue} alt="flecheBlue"/>
         </div>
       </div>
       </div>
       <div className='ShowCar'>
         <img className="ShowCar2" src={ShowCar2} alt="modele" />
+      </div>
+      <div className='configuration'>
+        <img className='choiceColor' src={blanc.jpg} alt=''/>
+        <img className='choiceColor' src={bleu.jpg} alt=''/>
+        <img className='choiceColor' src={noir.jpg} alt=''/>
       </div>
 
     </div>
